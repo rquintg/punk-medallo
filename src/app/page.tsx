4979d9c29a534ad9147bce3448df57f939b1c832
Ricardo Q. <ricardoqg41@icloud.com>
2026-08-21 21:37:57 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,12 +41,12 @@ export default async function Home() {
     <>
       <section className="relative w-full overflow-hidden">
         <div
-          className="relative z-0 flex items-start justify-center h-[44vh] w-screen text-white pt-[64px] mb-[39vh] max-md:mb-[41vh] xl:h-[58vh] xl:mb-[10vh] ml-[calc(-50vw+50%)] bg-cover bg-center bg-no-repeat before:absolute before:inset-0 before:bg-gradient-to-br before:from-black/50 before:to-black/60 before:z-[-1] before:pointer-events-none"
+          className="relative z-0 flex items-start justify-center h-[44vh] w-screen text-white pt-[56px] mb-[39vh] max-md:mb-[41vh] xl:h-[58vh] xl:mb-[10vh] ml-[calc(-50vw+50%)] bg-cover bg-center bg-no-repeat before:absolute before:inset-0 before:bg-gradient-to-br before:from-black/50 before:to-black/60 before:z-[-1] before:pointer-events-none"
           style={{ backgroundImage: "url('/images/fondo.jpeg')" }}
         >
           <div className="flex flex-col items-center justify-center mx-auto p-4 w-full max-w-[600px]">
             <h1 className="sr-only">Punk Medallo</h1>
-            <div className="relative mt-3 w-[90%] max-w-[450px] aspect-[2/1]">
+            <div className="relative w-[90%] max-w-[450px] aspect-[2/1]">
               <Image
                 src={logoUrl ?? LOGO_DEFAULT}
                 alt="punk medallo"
